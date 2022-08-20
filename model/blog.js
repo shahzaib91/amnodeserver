7ae29f6985d5cc3f453f12e0f0c8ef2dbe4c02ab
Shahzaib <Shahzaib.2377@gmail.com>
2022-08-20 15:54:37 +0500

    db.js file added Created blog insertion function in model

--- model/blog.js
+++ model/blog.js
@@ -1,5 +1,15 @@
+const db = require('../db');
+const moment = require('moment');
+
 // create article function
 exports.modelCreateBlog = (title, content) =>
 {
-    return {status:false, message:"Failed to create record"};
+    var sql = "INSERT INTO articles (title,content,timestamp) VALUES('"+title+"','"+content+"','"+moment().format('YYYY-MM-DD HH:mm:ss')+"')";
+    
+    db.con.query(sql, (err, result)=>{
+        if(err)
+            return {status:false, message:"DB Error: "+err};
+    });
+
+    return {status:true, message:""};
 }
