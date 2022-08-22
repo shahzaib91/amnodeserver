ae91e7b7a43b4ad9195f837e2b9b492bc7069c4f
Shahzaib <Shahzaib.2377@gmail.com>
2022-08-22 23:49:41 +0500

    Tree is made server side

--- model/comment.js
+++ model/comment.js
@@ -29,10 +29,28 @@ exports.createComment = (user_name, content, parent, belongs_to, res) =>
 exports.readComments = (blog_id, res) =>
 {
     const sql = "SELECT * FROM comments WHERE belongs_to=? ORDER BY timestamp DESC";
-    db.con.query(sql,[blog_id],(err,result,field)=>{
+    db.con.query(sql,[blog_id],(err,result,field)=>
+    {
         if(err)
+        {
             res.status(400).send({status:false, message:""+err});
+        }
         else
-            res.status(200).send({status:true, message:"Records", records: result});
+        {
+            const sortedArr = createDataTree(result);
+            res.status(200).send({status:true, message:"Records", records: sortedArr});
+        }
     })
 }
+
+// helper function to create proper tree
+const createDataTree = dataset => {
+    const hashTable = Object.create(null);
+    dataset.forEach(aData => hashTable[aData.id] = {...aData, childNodes: []});
+    const dataTree = [];
+    dataset.forEach(aData => {
+      if(aData.parent) hashTable[aData.parent].childNodes.push(hashTable[aData.id])
+      else dataTree.push(hashTable[aData.id])
+    });
+    return dataTree;
+};
